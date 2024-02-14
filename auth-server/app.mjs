9c86477c30cc145f1421636cfc59e06b982ad1e6
thenamevishnu <mail.vishnumk@gmail.com>
2024-02-14 13:53:55 +0530

    updates in api: server wake up

--- auth-server/app.mjs
+++ auth-server/app.mjs
@@ -3,6 +3,8 @@ import env from "dotenv"
 import cors from "cors"
 import authRouter from "./Routes/auth.route.mjs"
 import * as db from "./Database/db.mjs"
+import nodecron from "node-cron"
+import axios from "axios"
 
 env.config()
 db.connect(process.env.AUTH_DB)
@@ -14,6 +16,15 @@ app.use(cors({
     methods: "*"
 }))
 app.use(express.json())
+
+app.get("/status", async (req, res) => {
+    return res.status(200).send({ status: "OK"})
+})
+
+nodecron.schedule("* * * * *", async () => {
+    const { data } = await axios.get(`${process.env.SERVER}/status`)
+    console.log(data.status);
+})
 
 app.use("/", authRouter)
 
